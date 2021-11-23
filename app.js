e27e5bf6e2b68726022eff33172d3a00accfa6f0
Jo <88668777+jonars99@users.noreply.github.com>
2021-11-23 11:42:26 +0000

    required components done and finished styling

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var allToDos = function () {
     success: function (result, textStatus) {
       $('#note-list').empty();
       result.tasks.forEach(function (task) {
-        $('#note-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '/>');
+        $('#note-list').append('<div class="row todo"><input type="checkbox" class="complete col-auto ms-4" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '/><p class="col-10 my-2">' + task.content + '</p><button class="btn col-auto delete" data-id="' + task.id + '">X</button>');
       });
     },
     error: function (request, textStatus, errorMessage) {
@@ -64,6 +64,20 @@ var completeTask = function (id) {
   });
 }
 
+var activeTask = function (id) {
+  $.ajax({
+    type: 'PUT',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_active?api_key=204',
+    dataType: 'json',
+    success: function (result, textStatus) {
+      allToDos();
+    },
+    error: function (result, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+}
+
 $('document').ready(function () {
 
   $('#add-task').on('submit', function (event) {
@@ -81,8 +95,11 @@ $('document').ready(function () {
     if (this.checked) {
       completeTask(id);
     }
+    else {
+      activeTask(id);
+    }
   });
 
   allToDos();
 
-})
+});
